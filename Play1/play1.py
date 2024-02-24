9f2c0dad17c9352aba9bacb82e7ebd3d6c8408be
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-25 05:35:38 +0800

    update herE

--- Play1/play1.py
+++ Play1/play1.py
@@ -1,10 +1,12 @@
 # print("Hello World") ;
 
-userName = "Jhon Mark " ; # string
-age = 21 ; # 
-money = 50.00 ; # Decimal 
+# userName = "Jhon Mark " ; # string
+# age = 21 ; # 
+# money = 50.00 ; # Decimal 
 
-print("Hello I'm ", userName) ;
+# print("Hello I'm ", userName) ;
 
+userName = input("Enter your name:") ;
+print(userName) ;
 
 
