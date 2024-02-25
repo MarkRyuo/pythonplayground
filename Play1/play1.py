dfb0ae8a9f4de007b26fe1960739fbdf431c8a8b
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-02-26 05:20:32 +0800

    udpat eheRE

--- Play1/play1.py
+++ Play1/play1.py
@@ -48,7 +48,7 @@ userName  = str(input("Enter your Username: ")) ;
 passWord = str(input("Enter your password: ")) ;
 age = ["20", "21", "22","23"] ; # This is list 
 
-print("Ohayo! ", userName, " Your Age is ", age[1]) ;
+print("Ohayo! ", userName, " Your Age is ", age[-4]) ;
 
 # Todo In the list 
 #Varible   0     1     2    3  + Index
