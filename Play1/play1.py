7db673cf485e2739e0d08066585324677a87e9e2
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-09 10:03:13 +0800

    udpate ehrE

--- Play1/play1.py
+++ Play1/play1.py
@@ -232,15 +232,14 @@
 
 
 user_Name = "Jhon Mark" 
-user = input("Are you Jhon Mark?: ")
+user = input("Enter Your Name: ")
 
 if user == "Jhon Mark" : 
   user_Identity = input("Are you real Jhon Mark")
   if user_Identity == "Yes" :
     print("Are you real Jhon Mark! Sorry fo waiting please come in")
   else : 
-    print("Dont come in")
-else :
+    print("Dont come in") 
 
 
 
