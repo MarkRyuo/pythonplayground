3e02477a16fe4a0a3e2045fd9298821733b38ca1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-10 10:35:26 +0800

    udpate herE

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ good = int(input("How many good? "))
 
 # Enter Evil Bob and Elise
 
-if user_Name == "Bob" or user_Name == "Elise" : 
+if user_Name == "bob" or user_Name == "Elise" : 
   if good < 4 : 
    print("Dont Come In")
    exit()  
 elif user_Name == "Bob" and good > 4 :
   print("Hello Not evil Bob your welcome here")
+
 else :
   print("Please come in") 
