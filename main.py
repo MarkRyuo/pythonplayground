bc0105df87b5e216a113c4ae9e1e6363da832ce9
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-10 10:26:40 +0800

    udpateherE

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # todo Logical Operator  (and, or , not )
 
 user_Name = input("Enter your username: ")
+
+# Enter Evil Bob and Elise
+
+if user_Name == "Bob" and user_Name == "Elise" :
+  print("Dont Come In") 
