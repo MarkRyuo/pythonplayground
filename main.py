b92458c1b6b30db32e2d317d18e0c6325ab6bc59
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-10 10:33:51 +0800

    udpateehrE

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # todo Logical Operator  (and, or , not )
 
 user_Name = input("Enter your username: ")
-good = int(1input("How many good? "))
+good = int(input("How many good? "))
 
 # Enter Evil Bob and Elise
 
-if user_Name == "Bob" or user_Name == "Elise" :
-  user_Identity = int(input("How many good? ")) 
-  if user_Identity > 4 : 
+if user_Name == "Bob" or user_Name == "Elise" : 
+  if good < 4 : 
    print("Dont Come In")
    exit()  
-elif user_Name == "Bob" and 
+elif user_Name == "Bob" and good > 4 :
+  print("Hello Not evil Bob your welcome here")
 else :
   print("Please come in") 
