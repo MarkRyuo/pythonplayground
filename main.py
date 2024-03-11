4791f68ba1c606a8523e8e3408d43fdfd88ea3ce
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-11 19:41:11 +0800

    udpate herE

--- main.py
+++ main.py
@@ -28,4 +28,7 @@ user_Pass = input("Enter your password: ")
 # Yohan, Gilbert, Bernard 
 
 if user_Name == "Yohan" or user_Name == "Gilbert" or user_Name == "Bernard" :
-  print(f"{user_Name}, Your not welcome here!!!!")
+  user_Identity = print("Are you Evil? ")
+  if user_Identity == "Yes" :
+   print(f"{user_Name}, Your not welcome here!!!!")
+
