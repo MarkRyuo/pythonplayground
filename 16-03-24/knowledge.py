949160b500cd0dcede982347ffcf431529940f4d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-16 09:04:45 +0800

    udpaetehrE

--- 16-03-24/knowledge.py
+++ 16-03-24/knowledge.py
@@ -3,12 +3,13 @@
 user_Name = print("Enter your name: ")
 user_Age = int(print("What is your age: "))
 
-if user_Age < 10 : 
+if user_Age <= 10 : 
   print(f"Your {user_Age}, you're too young to drink coffee ")
   exit() 
 else :
   print(f"Hello {user_Name}, Welcome to the ARC mini Coffee shop \n \n")
-
+  # If age is greater to 10 tutuloy  
+  
 list_of_coffee = (
   "Espresso", 
   "Cappuccino", 
