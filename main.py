8c9ece0025defdbec3b5f7c0ca9dd3adcea62454
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-16 08:14:02 +0800

    update ehrE

--- main.py
+++ main.py
@@ -16,75 +16,76 @@
 #   print("Please come in") 
 
 
-print("Welcome to Arc Coffee Shop \n")
+# print("Welcome to Arc Coffee Shop \n")
 
-# Create a log-in
-# Username && Password 
+# # Create a log-in
+# # Username && Password 
 
-user_Name = input("Enter your username: ")
-user_Pass = input("Enter your password: ")
+# user_Name = input("Enter your username: ")
+# user_Pass = input("Enter your password: ")
 
-# List of Evil Customer 
-# Lily, Gilbert, Bernard 
+# # List of Evil Customer 
+# # Lily, Gilbert, Bernard 
 
-if user_Name == "Lily" or user_Name == "Gilbert" or user_Name == "Bernard" : # using Logical Operator
-    user_Identity = input("Are you Evil? (Yes/No) : ")
-    if user_Identity == "Yes" or user_Identity == "No" :
-      user_Clarify = int(input("How many good ways do you have?: "))
-      if user_Clarify < 3 :
-       print(f"{user_Name}, You're not welcome here!!!!")
-       exit()
-      else :
-        print(f"{user_Name}, Sorry for mistake your good your welcome here")
-else :
-  print(f"Hello {user_Name}, welcome the arc coffee shop!\n \n")
+# if user_Name == "Lily" or user_Name == "Gilbert" or user_Name == "Bernard" : # using Logical Operator
+#     user_Identity = input("Are you Evil? (Yes/No) : ")
+#     if user_Identity == "Yes" or user_Identity == "No" :
+#       user_Clarify = int(input("How many good ways do you have?: "))
+#       if user_Clarify < 3 :
+#        print(f"{user_Name}, You're not welcome here!!!!")
+#        exit()
+#       else :
+#         print(f"{user_Name}, Sorry for mistake your good your welcome here")
+# else :
+#   print(f"Hello {user_Name}, welcome the arc coffee shop!\n \n")
 
-# List of coffee's 
+# # List of coffee's 
 
-# I use Array here g
-list = ["Espresso", "Cappuccino", "Latte", "Americano", "Mocha"] 
+# # I use Array here g
+# list = ["Espresso", "Cappuccino", "Latte", "Americano", "Mocha"] 
 
-print(f"This is the list of Coffee's {list}\n") 
+# print(f"This is the list of Coffee's {list}\n") 
 
-choose = input("What Coffee you love?: ")
-quantity = int(input("How many?: "))
+# choose = input("What Coffee you love?: ")
+# quantity = int(input("How many?: "))
 
-# I use if statement and elif 
+# # I use if statement and elif 
 
-if choose == "Espresso" :
-  print(f"Your order is {list[0]}")
-  price = 50 
-  total = price * quantity
-  print(f"The price of {list[0]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00") 
-elif choose == "Cappuccino" :
-  print(f"Your order is {list[1]}")
-  price = 50 
-  total = price * quantity
-  print(f"The price of {list[1]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00") 
-elif choose == "Latte" :
-  print(f"Your order is {list[2]}")
-  price = 50 
-  total = price * quantity
-  print(f"The price of {list[2]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
-elif choose == "Americano" : 
-  print(f"Your order is {list[3]}")
-  price = 50 
-  total = price * quantity
-  print(f"The price of {list[3]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
-elif choose == "Mocha" :
-  print(f"Your order is {list[4]}")
-  price = 50 
-  total = price * quantity
-  print(f"The price of {list[4]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
-else :
-  print("null") 
+# if choose == "Espresso" :
+#   print(f"Your order is {list[0]}")
+#   price = 50 
+#   total = price * quantity
+#   print(f"The price of {list[0]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00") 
+# elif choose == "Cappuccino" :
+#   print(f"Your order is {list[1]}")
+#   price = 50 
+#   total = price * quantity
+#   print(f"The price of {list[1]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00") 
+# elif choose == "Latte" :
+#   print(f"Your order is {list[2]}")
+#   price = 50 
+#   total = price * quantity
+#   print(f"The price of {list[2]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
+# elif choose == "Americano" : 
+#   print(f"Your order is {list[3]}")
+#   price = 50 
+#   total = price * quantity
+#   print(f"The price of {list[3]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
+# elif choose == "Mocha" :
+#   print(f"Your order is {list[4]}")
+#   price = 50 
+#   total = price * quantity
+#   print(f"The price of {list[4]} is {price} per 1 and you ordered {quantity}, the total is ₱{total}.00")
+# else :
+#   print("null") 
 
-# Todo Need multiple Elif need ✅
-#Todo Need Upgrade here 
-# todo update add if add 
+# # Todo Need multiple Elif need ✅
+# #Todo Need Upgrade here 
+# # todo update add if add 
   
-addAnother = input("Add Coffee: ")
+# addAnother = input("Add Coffee: ")
 # list.append(addAnother) #add in last
 # list.insert(3, addAnother) # add in index 3 the new add coffee 
  
-print(list)
+# print(list)
+
