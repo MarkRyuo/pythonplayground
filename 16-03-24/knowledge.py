675218ec0ca4112e9d1789418ad87fecd2fa302c
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-16 08:54:38 +0800

    update ehrE

--- 16-03-24/knowledge.py
+++ 16-03-24/knowledge.py
@@ -1,4 +1,9 @@
 # todo all knowledge to Network Chunk 
 
 user_Name = print("Enter your name: ")
-  print(f"Hello {user_Name}, Welcome to the ARC mini Coffee shop")
+user_Age = int(print("What is your age: "))
+
+print(f"Hello {user_Name}, Welcome to the ARC mini Coffee shop \n")
+
+list_of_coffee = []
+
