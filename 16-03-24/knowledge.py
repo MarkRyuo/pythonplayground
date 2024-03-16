6aad0994c1aa3aa7182dc02076f6fadfd4ceaec0
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-16 08:56:47 +0800

    udpate ehrE

--- 16-03-24/knowledge.py
+++ 16-03-24/knowledge.py
@@ -3,6 +3,9 @@
 user_Name = print("Enter your name: ")
 user_Age = int(print("What is your age: "))
 
+if user_Age < 10 : 
+  print(f"Your {user_Age}, too young ") 
+
 print(f"Hello {user_Name}, Welcome to the ARC mini Coffee shop \n")
 
 list_of_coffee = []
