c9a468affaf624863ff663603f395ad8303b19a1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-17 09:24:15 +0800

    updateherE

--- 16-03-24/knowledge.py
+++ 16-03-24/knowledge.py
@@ -67,4 +67,5 @@ elif Order == "Americano" :
 elif Order == "Mocha" :
   price = 50 
   total = price * quantity 
-  
+  print(f"Your order is {quantity} pcs of {list_of_coffee[4]}, total of {total}. ")
+else 
