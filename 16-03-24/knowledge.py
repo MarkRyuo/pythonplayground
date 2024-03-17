5226780a2be86f4e69d4c81f6c3f40eebc3d94b7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-17 09:16:27 +0800

    update herE

--- 16-03-24/knowledge.py
+++ 16-03-24/knowledge.py
@@ -56,4 +56,5 @@ elif Order == "Cappuccino" :
   price = 35 
   total = price + quantity 
   print(f"Your order is {quantity} pcs of {list_of_coffee[1]}, total of {total}. ")
+elif Order == "Latte" :
   
