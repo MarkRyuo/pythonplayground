98c082ad31866ebbd1e5c372f28bbee3670216f4
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-21 21:35:12 +0800

    udpateehrE

--- main2.py
+++ main2.py
@@ -26,9 +26,9 @@ list_of_Coffee = {
 
 coffee_List = "" 
 #Do you want to see the list of Coffee 
-while coffee_List == 0 :
+while len(coffee_List) == 0 :
   coffee_List = input("Do you want to see the list of coffee (y/n)")  
-  
+
 print(f"Here the coffee list {list_of_Coffee} .")
 
 
