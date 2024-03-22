a9348d4140eaa702cf4f46421f07cde2c9d0ee92
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-22 09:07:18 +0800

    udpate ehrE

--- main2.py
+++ main2.py
@@ -12,23 +12,23 @@
 
 customer_name = "" # Declare ng empty variable 
 
-while len(customer_name) == 0 :
-  customer_name = input("Enter your name: ")
+# while len(customer_name) == 0 :
+#   customer_name = input("Enter your name: ")
 
-print(f"Hello {customer_name}, Welcome to Arc Coffee shop")
+# print(f"Hello {customer_name}, Welcome to Arc Coffee shop")
 
-list_of_Coffee = {
-  "Barako",
-  "Latte" ,
-  "Mocha",
-  "Americano"
-}
+# list_of_Coffee = {
+#   "Barako",
+#   "Latte" ,
+#   "Mocha",
+#   "Americano"
+# }
 
-coffee_List = "" 
-#Do you want to see the list of Coffee 
-while len(coffee_List) == 0 :
-  coffee_List = input("Do you want to see the list of coffee (y/n): ")  
+# coffee_List = "" 
+# #Do you want to see the list of Coffee 
+# while len(coffee_List) == 0 :
+#   coffee_List = input("Do you want to see the list of coffee (y/n): ")  
 
-print(f"Here the coffee list {list_of_Coffee} .")
+# print(f"Here the coffee list {list_of_Coffee} .")
 
 
