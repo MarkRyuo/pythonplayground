84f6ecad230c8d3d695e877b214e519d5f5a979c
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-23 20:41:22 +0800

    udpate herE

--- main2.py
+++ main2.py
@@ -93,7 +93,11 @@
 #     user_Name = input("Enter your name: ")
 #   return user_Name 
 
-def username() :
+name = input("Enter your name")
+
+def username(name) :
+  
+  while len(name) == 0
   
 useras = username() 
 
