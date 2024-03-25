781ad9806056808532ac457facc97d68b5837a66
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-25 17:41:12 +0800

    udpate here

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 
 # Todo create log-in for user, admin and staff / employee 
 
-user = ["user1", "user2", "user3", "user4", "user5"]
+list_of_user = ["user1", "user2", "user3", "user4", "user5"] # List 
+
+admin = ("admin1", "admin2", "admin3", "admin4")
 
 def user () :
   
