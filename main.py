d73fcb126bf0fa5da9394b0de092faebf2bfc54a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-03-28 09:08:27 +0800

    udpate ehrE

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-# Todo create a coffee shop 
+# # Todo create a coffee shop 
 
 
-# Todo create log-in for user, admin and staff / employee 
+# # Todo create log-in for user, admin and staff / employee 
 
-list_of_user = [
-  "user1",
-  "user2",
-  "user3", 
-  "user4", 
-  "user5"
-] # List 
+# list_of_user = [
+#   "user1",
+#   "user2",
+#   "user3", 
+#   "user4", 
+#   "user5"
+# ] # List 
 
-list_of_employee = [
-  "user1",
-  "user2",
-  "user3",
-  "user4", 
-  "user5"
-] # List 
+# list_of_employee = [
+#   "user1",
+#   "user2",
+#   "user3",
+#   "user4", 
+#   "user5"
+# ] # List 
 
-list_of_admin = (
-  "admin1",
-  "admin2",
-  "admin3",
-  "admin4"
-) # Tuple
+# list_of_admin = (
+#   "admin1",
+#   "admin2",
+#   "admin3",
+#   "admin4"
+# ) # Tuple
 
-userin = ""
+# userin = ""
 
-def userOf() :
-  while userin == 0 :
-    user = input("Are you ? Pick 1: (user, staff, Employee): ")
-  return user 
+# def userOf() :
+#   while userin == 0 :
+#     user = input("Are you ? Pick 1: (user, staff, Employee): ")
+#   return user 
 
-userout = userOf()
+# userout = userOf()
 
-if userout == "user" :
-  print("Log in to user")
-  username = input("Enter your username: ")
-  password = input("Enter your password: ")
-  # if username == "user1" :
+# if userout == "user" :
+#   print("Log in to user")
+#   username = input("Enter your username: ")
+#   password = input("Enter your password: ")
+#   # if username == "user1" :
     
