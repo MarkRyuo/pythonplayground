2126e8e95f2e665b337d2608ead6cc72ee52fd93
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-02 19:18:20 +0800

    updta eheRE

--- Play/play.py
+++ Play/play.py
@@ -1,14 +1,15 @@
-person = {
-  "name": "Jhon Mark",
-  "age" : 21,
-  "online" : True,
-  "askOf" : lambda : print("Konichiwa Jhon Mark Desu!")
+# person = {
+#   "name": "Jhon Mark",
+#   "age" : 21,
+#   "online" : True,
+#   "askOf" : lambda : print("Konichiwa Jhon Mark Desu!")
 
-}
+# }
 
-def user_Person(person) :
-  login = person["name"]
-  online = person["online"]
-  print(f"Hey, you log in successfully user {login} and you are online: {online}")
+# def user_Person(person) :
+#   login = person["name"]
+#   online = person["online"]
+#   print(f"Hey, you log in successfully user {login} and you are online: {online}")
   
-user_Person(person) 
+# user_Person(person) 
+
