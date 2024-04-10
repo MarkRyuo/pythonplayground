60d8ad7a0c51ed00ea1d57a268a6aad856a94eee
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2024-04-10 22:40:26 +0800

    udpae herE

--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 # set {} - Unchangeable 
 # dict {"name": "Jhon Mark"} - Changeable 
 
-thislist  = ["apple", "mango", "cherry"]
-def thisislist(thislist) :
+def thisislist() :
+  thislist  = ["apple", "mango", "cherry"]
   print(f"List of fruits: {thisislist}")
 #add item in list 
   addinlist = input("Enter a fruit: ")
   thislist.append(addinlist)
-  print(f"New list of fruits: {thisislist} ")
+  print(f"New list of fruits: {thislist} ")
 
-thisislist(thislist)
+thisislist()
